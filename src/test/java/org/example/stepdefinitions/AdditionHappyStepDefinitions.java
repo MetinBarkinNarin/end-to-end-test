c5a8957fe5ea66f9e12e0663dadc9961b7131d0e
MetinBarkinNarin <74645435+MetinBarkinNarin@users.noreply.github.com>
2021-01-22 23:03:19 +0300

    Add files via upload

---
+++ src/test/java/org/example/stepdefinitions/AdditionHappyStepDefinitions.java
@@ -0,0 +1,66 @@
+package org.example.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.example.testcontainer.CalculatorContainerService;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@ContextConfiguration
+public class AdditionHappyStepDefinitions {
+
+    @Autowired
+    private CalculatorContainerService calculatorContainerService;
+
+    static {
+        setRyukDisabledInEnv();
+    }
+
+    @DynamicPropertySource//This annotation lets you override Spring configuration properties programmatically
+    static void properties(DynamicPropertyRegistry registry) {
+        // registry.add(key, value);
+    }
+
+    @Given("{} and {} are given")
+    public void givenNumbers(Integer numberOne, Integer numberTwo) {
+        System.out.println("Given " + numberOne + " " + numberTwo + " " + this.calculatorContainerService.getCalculatorContainer().getDockerImageName());
+    }
+
+    @When("Mutlu wants to add those two numbers")
+    public void addingNumbers() {
+        System.out.println("When");
+    }
+
+    @Then("Mutlu should see {} result")
+    public void checkingResultNumber(Integer result) {
+        System.out.println("Then " + result);
+
+        this.calculatorContainerService.stopContainers();
+    }
+    private static void setRyukDisabledInEnv() {
+        try {
+            Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+            Field theCaseInsensitiveEnvironment = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
+            theCaseInsensitiveEnvironment.setAccessible(true);
+            Map<String, String> caseEnv = (Map<String, String>) theCaseInsensitiveEnvironment.get(null);
+            caseEnv.put("TESTCONTAINERS_RYUK_DISABLED", "true");
+        } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/org/example/testcontainer/CalculatorContainerService.java
@@ -0,0 +1,118 @@
+package org.example.testcontainer;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.File;
+
+@Service
+public class CalculatorContainerService {
+    private static final DockerImageName ADDITION_SERVICE = DockerImageName.
+            parse("mbarkin26/addition-service:latest")
+            .asCompatibleSubstituteFor("addition-service");
+    private static final DockerImageName SUBTRACTION_SERVICE = DockerImageName
+            .parse("mbarkin26/subtraction-service:latest")
+            .asCompatibleSubstituteFor("subtraction-service");
+    private static final DockerImageName CALCULATOR_SERVICE = DockerImageName
+            .parse("mbarkin26/calculator-service:latest")
+            .asCompatibleSubstituteFor("calculator-service");
+//    private static final DockerImageName SELENIUM_SERVICE = DockerImageName
+//            .parse("selenium/node-chrome:latest")
+//            .asCompatibleSubstituteFor("node-chrome");
+
+    private Network network;
+    private GenericContainer<?> additionContainer;
+    private GenericContainer<?> subtractionContainer;
+    private GenericContainer<?> calculatorContainer;
+    private BrowserWebDriverContainer<?> chromeContainer;
+
+    public CalculatorContainerService() {
+        network = Network.newNetwork();
+        additionContainer = new GenericContainer<>(ADDITION_SERVICE)
+                .withExposedPorts(8070)
+                .withNetwork(network)
+                .withEnv("SERVER_PORT", "8070")
+                .withEnv("SERVER_ADDRESS", "localhost");
+
+        subtractionContainer = new GenericContainer<>(SUBTRACTION_SERVICE)
+                .withExposedPorts(8071)
+//                .withExtraHost("subtraction-service","10.150.17.73")
+                .withNetwork(network)
+                .withEnv("SERVER_PORT", "8071")
+                .withEnv("SERVER_ADDRESS", "localhost");
+
+        calculatorContainer = new GenericContainer<>(CALCULATOR_SERVICE)
+                .withExposedPorts(8072)
+                .withNetwork(network)
+                .withEnv("SERVER_PORT", "8072")
+                .withEnv("SERVER_ADDRESS", "localhost");
+
+        chromeContainer = new BrowserWebDriverContainer<>()
+                .withNetwork(network)
+                .withNetworkAliases("chrome")
+                .withCapabilities(DesiredCapabilities.chrome())
+                .withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL, new File("./target/"));
+//                .withExposedPorts(8072)
+//                .withNetwork(network)
+//                .withEnv("SERVER_PORT", "8072")
+//                .withEnv("SERVER_ADDRESS", "localhost");
+
+        additionContainer.start();
+        subtractionContainer.start();
+        calculatorContainer.start();
+        chromeContainer.start();
+    }
+
+    public void stopContainers(){
+        additionContainer.close();
+        subtractionContainer.close();
+        calculatorContainer.close();
+        chromeContainer.close();
+        network.close();
+    }
+
+    public Network getNetwork() {
+        return network;
+    }
+
+    public void setNetwork(Network network) {
+        this.network = network;
+    }
+
+    public GenericContainer<?> getAdditionContainer() {
+        return additionContainer;
+    }
+
+    public void setAdditionContainer(GenericContainer<?> additionContainer) {
+        this.additionContainer = additionContainer;
+    }
+
+    public GenericContainer<?> getSubtractionContainer() {
+        return subtractionContainer;
+    }
+
+    public void setSubtractionContainer(GenericContainer<?> subtractionContainer) {
+        this.subtractionContainer = subtractionContainer;
+    }
+
+    public GenericContainer<?> getCalculatorContainer() {
+        return calculatorContainer;
+    }
+
+    public BrowserWebDriverContainer<?> getChromeContainer() {
+        return chromeContainer;
+    }
+
+    public void setChromeContainer(BrowserWebDriverContainer<?> chromeContainer) {
+        this.chromeContainer = chromeContainer;
+    }
+
+    public void setCalculatorContainer(GenericContainer<?> calculatorContainer) {
+        this.calculatorContainer = calculatorContainer;
+    }
+}
