735a3148353ae7da8274afc508f5862570d54898
MetinBarkinNarin <mbarkin.narin@gmail.com>
2021-01-24 22:52:41 +0300

    code update deneme 2

--- src/test/java/org/example/testcontainer/CalculatorContainerService.java
+++ src/test/java/org/example/testcontainer/CalculatorContainerService.java
@@ -34,6 +34,7 @@ public class CalculatorContainerService {
 
     public static GenericContainer<?> additionContainer =
             new GenericContainer<>(ADDITION_SERVICE)
+                    .withPrivilegedMode(true)
                     .withExposedPorts(8070)
                     .withNetwork(Network.newNetwork())
                     .withNetworkAliases("docker")
